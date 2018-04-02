052aca7f96b70ce58714cbf4355416434b2917f9
Zoilo Bulawan Jr <37639405+judiboy@users.noreply.github.com>
2018-04-02 09:59:05 +0800

    Update register.php

--- app/register.php
+++ app/register.php
@@ -9,9 +9,11 @@
                <a href="register_driver.php"> <button type="button" class="btn btn-primary">Be a Driver</button></a>
 			   <a href="register.php"> <button type="button" class="btn btn-primary">Get a Ride</button></a>      
 	</header>
-	<div style="float:left; width:20%;">
-	<img src="../app/assets/images/upoulogo.png" width="200" height="200"/>
-    <h1>Greetings, Earthlings!</h1>
+	<div class="col-md-2 col-md-offset-2">
+	<div style="float:left; width:40%;">
+	   <img src="../app/assets/images/upoulogo.png" width="200" height="200"/>
+           <h1>Greetings, Earthlings!</h1>
+	</div>
 	</div>
            <div class="col-md-4 col-md-offset-4">	
       <div class="row  h-100">
