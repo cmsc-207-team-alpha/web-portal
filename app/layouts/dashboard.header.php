4af6897318b85c912e4538689f0fdf9b2602a69c
Zoilo Bulawan Jr <37639405+judiboy@users.noreply.github.com>
2018-04-06 23:29:11 +0800

    Update dashboard.header.php

--- app/layouts/dashboard.header.php
+++ app/layouts/dashboard.header.php
@@ -7,6 +7,6 @@
   <link rel="stylesheet" href="../../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../../vendor/components/font-awesome/css/fontawesome-all.css">
   <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png"/>
-	<script src="../vendor/components/jquery/jquery.min.js"></script>
-	<script src="../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="../../vendor/components/jquery/jquery.min.js"></script>
+	<script src="../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="assets/stylesheets/dashboard.css">
