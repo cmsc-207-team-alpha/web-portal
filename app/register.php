e6e6796e8846c2feb8102f4544aef914e651ad54
Zoilo Bulawan Jr <37639405+judiboy@users.noreply.github.com>
2018-04-06 16:53:35 +0800

    Update register.php

--- app/register.php
+++ app/register.php
@@ -20,40 +20,37 @@
           </div>
 		 
               <h4 class="card-title">Register Passenger</h4>
-              <form method="POST">
+             <form method="POST">
                 <div class="form-group">
                   <div class="form-row">
-                    <div class="col-md-6 form-group">
+                   
                       <label for="email">Email</label>
                       <input class="form-control" name="email" id="email" type="text"  placeholder="Email">
                     </div>
-		<div class="form-group">
-                  <div class="form-row">
-                    <label for="username">Username</label>
-                      <input type="text" class="form-control" name="username" id="username" placeholder="Username"> 
-                  </div>
+                    
                 </div>
-                    <div class="col-md-6 form-group">
-                      <label for="password">Password</label>
-                      <input class="form-control" name="password" id="password" type="text"  placeholder="Password">
-                    </div>
-                  </div>
-                </div>
-                
+				
                 <div class="form-group">
                   <div class="form-row">
-                  <label for="password">Password</label>
-                    <input type="text" class="form-control" name="confirmpassword" id="confirmpassword" placeholder="Confirm Password">
+                    <label for="password">Password</label>
+                      <input type="text" class="form-control" name="username" id="username" placeholder="Password"> 
+                  </div>
+                </div>
+				
+                <div class="form-group">
+                  <div class="form-row">
+                  <label for="password">Confirm Password</label>
+                    <input type="text" class="form-control" name="password" id="password" placeholder="Confirm Password">
                   </div>
                 </div>
 
 
-                <div class="form-group no-margin">
+                <div class="form-group">
                   <button type="submit" class="btn btn-success btn-block">
                     Register
                   </button>
                 </div>
-                <div class="form-group no-margin">
+                <div class="form-group ">
                   Already have an account? <a href="login.php">Login Passenger</a> | <a href="login_driver.php">Login Driver</a>
                 </div>
               </form>
