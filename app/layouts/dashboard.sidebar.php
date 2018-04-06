23b6352a1a2e9e364537f30d9d79e504782c7334
Zoilo Bulawan Jr <37639405+judiboy@users.noreply.github.com>
2018-04-07 00:00:05 +0800

    Update dashboard.sidebar.php

--- app/layouts/dashboard.sidebar.php
+++ app/layouts/dashboard.sidebar.php
@@ -37,7 +37,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="reports.php">
+                <a class="nav-link" href="reports/index.php">
                   <span class="fa fa-fw fa-chart-line"></span>
                   Reports
                 </a>
