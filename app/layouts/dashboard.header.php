40882d1ea4524b84767fe09474f2b9e2570e761a
Zoilo Bulawan Jr <37639405+judiboy@users.noreply.github.com>
2018-04-06 23:09:24 +0800

    Update dashboard.header.php

--- app/layouts/dashboard.header.php
+++ app/layouts/dashboard.header.php
@@ -4,9 +4,9 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <title>Team Alpha</title>
-	<link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../vendor/font-awesome/css/fontawesome-all.css">
+  <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../vendor/components/font-awesome/css/fontawesome-all.css">
   <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png"/>
-	<script src="../vendor/jquery/jquery.min.js"></script>
-	<script src="../vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="../vendor/components/jquery/jquery.min.js"></script>
+	<script src="../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="assets/stylesheets/dashboard.css">
