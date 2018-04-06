17a1dd8bb03f7e5b85ac3444d7a11ba30ad46d7b
Zoilo Bulawan Jr <37639405+judiboy@users.noreply.github.com>
2018-04-07 01:07:07 +0800

    Update login.php

--- app/login.php
+++ app/login.php
@@ -29,7 +29,7 @@
                   <div class="input-group-prepend">
                   <div class="input-group-text"><i class="fa fa-lg fa-user"></i></div>
                   </div>
-                  <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+                  <input type="text" class="form-control" name="email" id="email" placeholder="Username">
                   </div>
                 </div>
                 <div class="form-group">
@@ -61,7 +61,36 @@
                   Don't have an account? <a href="register.php">Create One</a>
                 </div>
               </form>
-
+<?php
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $data = array(
+        'email' => $_POST['email'],
+        'password' => $_POST['password'],
+    );
+    $data_string = json_encode($data);
+    # Create a connection
+    $url = 'https://cmsc-207-team-alpha.000webhostapp.com/api/admin/authenticate.php';
+    $ch = curl_init($url);
+    # Setting our options
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_string)));
+    # Get the response
+    $response = json_decode(curl_exec($ch));
+    curl_close($ch);
+    if (property_exists($response, 'id')) {
+        echo "Successful response:<br/>";
+        echo "Message: " . $response->message . '<br/>';
+        echo "Id: " . $response->id;
+    } else {
+        echo "Error response:<br/>";
+        echo "Message: " . $response->message;
+    }
+}
+?>
       
        
      </div>
