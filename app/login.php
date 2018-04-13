f4de12250f0bf6b3f59f3f64bb67c27aa0f74d74
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-13 22:57:52 +0800

    Update login.php

--- app/login.php
+++ app/login.php
@@ -132,7 +132,7 @@ else {
           $("#result").removeClass();
             $('#result').addClass('alert alert-success');
             $('#result').html("Successful Message:" + response["message"] + ". ID: " + response["id"]);
-			window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/reports.php';
+			window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/dashboard.php';
           },
           error: function (response) {
           $("#result").removeClass();
