249ba3b7d3acd476d144475e4fb313b27f4379ab
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-14 16:15:06 +0800

    Update reset-password.php

--- app/reset-password.php
+++ app/reset-password.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
 <?php include_once("layouts/header.php") ?>
-<link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 </head>
 <body class="login-page">
   <section class="h-100">
@@ -13,26 +12,41 @@
               <img src="assets/images/logoteam-alpha.png">
             <a href="index.php">
           </div>
-
+          <div class="card-wrapper">
+          <div class="brand">
+            <a href="index.php">
+              <img src="assets/images/logoteam-alpha.png">
+            </a>
+          </div>
           <div class="card fat">
             <div class="card-body">
-              <h4 class="card-title">Forgot Password</h4>
+              <h4 class="card-title">Reset Password</h4>
               <div class="text-center">
-              <p>Please enter your email address to reset password.</p>
+              <p>Please enter the reset code and your new password.</p>
               </div>
               <form method="POST">
                 <div class="form-row form-group">
-                  <label for="email">Email Address</label>
-                  <input class="form-control" name="email" id="email" type="text"  placeholder="Email">
+                  <label for="email">Reset Code</label>
+                  <input class="form-control" name="code" id="code" type="text"  placeholder="Reset Code">
                 </div>
+		      
+		      <div class="form-row form-group">
+                  <label for="email">Password</label>
+                  <input class="form-control" name="password" id="password" type="password"  placeholder="Enter new password">
+                </div>
+		      
+		      <div class="form-row form-group">
+                  <label for="email">Confirm Password</label>
+                  <input class="form-control" name="conpassword" id="conpassword" type="password"  placeholder="Confirm new password">
+                </div>
+		      
+		      
                 <div class="form-row no-margin">
                   <button type="submit" class="btn btn-success btn-block">
-                    Reset Password
+                    Set New Password
                   </button>
                 </div>
-                <div class="margin-top20 text-center">
-                  <a href="login.php">Login now</a>
-                </div>
+
               </form>
             </div>
           </div>
@@ -43,38 +57,61 @@
       </div>
     </div>
   </section>
-  
+      
 <?php
+
 if(isset($_POST['submit']))
 {
-		$un=$_POST['email'];
-		$query=mysqli_query($conn, "SELECT * FROM admin WHERE email LIKE '$un' LIMIT 1")or die("Error Query");
-		if(mysqli_num_rows($query)>0)
-		{
-			$rv=mysqli_fetch_array($query);
-			$emailtoken=md5($rv['email']);
-			$email=$un;
-			$body="Reset your password by using this code:$emailtoken";
-				$emailsend=$email;
 	
-
-$subject="Password Reset";
-$alertmsg="A message was sent to your email";
-include('mailer.php');
-			
-		echo"<script>window.alert('An email has been sent, check the mail to change your password.');
-		window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/resetpass.php';</script>";
+		$code=$_POST['code'];
+		$password=$_POST['password'];
+		$conpassword=$_POST['conpassword'];
+		
+		if($password!=$conpassword)
+		{
+			echo"<script>window.alert('Password do not match');</script>";
 			
 		}
-		else
-		{
-			echo"<script>window.alert('Administrator account does not exist');</script>";
+		else{
 			
+		$query=mysqli_query($conn, "SELECT * FROM admin WHERE type=1")or die("Error Query");
+	    if(mysqli_num_rows($query)==0)
+	    {
+	        	echo"<script>window.alert('No available users');window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/login.php';</script>";
+	    }
+	    $rcd=0;
+		while($rvs=mysqli_fetch_array($query))
+		{
+			$rvtoken=md5($rvs['email']);
+			//echo"<script>window.alert($rvtoken vs $email);</script>";
+			if($rvtoken==$email)
+			{   $rcd=1;
+				$npass=md5($password);
+				$sps=mysqli_query($conn,"UPDATE admin SET password='$npass' WHERE email LIKE '$rvs[email]' LIMIT 1")or die("qs error");
+				if($sps)
+				{
+					echo"<script>window.alert('Successfully Updated your password, Try logging in again');window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/login.php';</script>";
+				}
+				else
+				{
+					echo"<script>window.alert('Successfully Updated your password, Try logging in again');</script>";
+					
+				}
+			}
+			
+		}
+		if($rcd==0)
+		{
+		    
+		    	echo"<script>window.alert('Invalid reset code');</script>";
+		}
+		
 		}
 		
 		
 		
 }
+
 
 ?>
 </body>
