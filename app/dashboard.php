d4eb217ded859728e69504a56f3cb3bf16a4e73d
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-14 13:28:02 +0800

    Update dashboard.php

--- app/dashboard.php
+++ app/dashboard.php
@@ -1,34 +1,10 @@
 <?php
-<script src="vendor/components/jquery/jquery.min.js"></script>
-	<script>
-	$.ajax({
-          type: "POST",
-          url: "/api/admin/authenticate.php",
-          }),
-if{
-          success: function (response) {
-          $("#result").removeClass();
-            $('#result').addClass('alert alert-success');
-			window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/dashboard.php';
-          }
-}else{
-          error: function (response) {
-          $("#result").removeClass();
-            $('#result').addClass('alert alert-danger');
-            window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/login.php';
-          },
-          contentType: "application/json; charset=UTF-8",
-          dataType: "json"
-		  
-}
-        });
 
-
-/*if(!isset($_SESSION['email']))
+if(!isset($_SESSION['email']))
 {
 	
 	echo"<script>window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/login.php';</script>";
-}*/
+}
 ?>
 
 
