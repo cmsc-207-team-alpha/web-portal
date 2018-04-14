d1f3356da5a0b1883b1999a52c51a8edbd4bfd23
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-14 16:07:50 +0800

    Add files via upload

---
+++ app/reset-password.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include_once("layouts/header.php") ?>
+<link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body class="login-page">
+  <section class="h-100">
+    <div class="container h-100">
+      <div class="row  h-100">
+          <div class="brand-lg">
+            <a href="index.php">
+              <img src="assets/images/logoteam-alpha.png">
+            <a href="index.php">
+          </div>
+
+          <div class="card fat">
+            <div class="card-body">
+              <h4 class="card-title">Forgot Password</h4>
+              <div class="text-center">
+              <p>Please enter your email address to reset password.</p>
+              </div>
+              <form method="POST">
+                <div class="form-row form-group">
+                  <label for="email">Email Address</label>
+                  <input class="form-control" name="email" id="email" type="text"  placeholder="Email">
+                </div>
+                <div class="form-row no-margin">
+                  <button type="submit" class="btn btn-success btn-block">
+                    Reset Password
+                  </button>
+                </div>
+                <div class="margin-top20 text-center">
+                  <a href="login.php">Login now</a>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="footer">
+            Copyright &copy; 2018 &mdash; UPOU-CMSC-207-Team-Alpha 
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+<?php
+if(isset($_POST['submit']))
+{
+		$un=$_POST['email'];
+		$query=mysqli_query($conn, "SELECT * FROM admin WHERE email LIKE '$un' LIMIT 1")or die("Error Query");
+		if(mysqli_num_rows($query)>0)
+		{
+			$rv=mysqli_fetch_array($query);
+			$emailtoken=md5($rv['email']);
+			$email=$un;
+			$body="Reset your password by using this code:$emailtoken";
+				$emailsend=$email;
+	
+
+$subject="Password Reset";
+$alertmsg="A message was sent to your email";
+include('mailer.php');
+			
+		echo"<script>window.alert('An email has been sent, check the mail to change your password.');
+		window.location='https://cmsc-207-team-alpha.000webhostapp.com/app/resetpass.php';</script>";
+			
+		}
+		else
+		{
+			echo"<script>window.alert('Administrator account does not exist');</script>";
+			
+		}
+		
+		
+		
+}
+
+?>
+</body>
+</html>
