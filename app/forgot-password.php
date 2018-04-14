ae26fa6fe5fc7b5d53621287a186eee50b55b574
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-14 11:36:10 +0800

    Add files via upload

--- app/forgot-password.php
+++ app/forgot-password.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include_once("layouts/login_header.php") ?>
+<?php include_once("layouts/header.php") ?>
 </head>
-<body>
-
-<?php include_once("layouts/login_nav.php")?>
-<div class="container-fluid">
- <div class="row content">
-    <div class="col-sm-4 sidenav" align="center"> 	
-	      	<?php include_once("layouts/login_sidebar.php")?>
-     </div>
- 
-	 <div class="col-sm-8">
-	 <div class="brand-lg">
-           
-              <img src="assets/images/logoteam-alpha.png" width="150" height="100">
-          
-          </div> 
+<body class="login-page">
+  <section class="h-100">
+    <div class="container h-100">
+      <div class="row  h-100">
+          <div class="brand-lg">
+            <a href="index.php">
+              <img src="assets/images/logoteam-alpha.png">
+            <a href="index.php">
+          </div>
+          <div class="card-wrapper">
+          <div class="brand">
+            <a href="index.php">
+              <img src="assets/images/logoteam-alpha.png">
+            </a>
+          </div>
+          <div class="card fat">
+            <div class="card-body">
               <h4 class="card-title">Forgot Password</h4>
               <div class="text-center">
-              <p>Enter your email address and we will send you instructions on how to reset your password.</p>
+              <p>Please enter your email address to reset your password</p>
               </div>
               <form method="POST">
                 <div class="form-row form-group">
-                  <label for="email">Email</label>
+                  <label for="email">Email Address</label>
                   <input class="form-control" name="email" id="email" type="text"  placeholder="Email">
                 </div>
                 <div class="form-row no-margin">
@@ -33,16 +35,17 @@
                   </button>
                 </div>
                 <div class="margin-top20 text-center">
-                  <a href="register.php"> Register an account</a> | <a href="login.php">Login now</a>
+                  <a href="register.php">Reset Password</a> | <a href="login.php">Login now</a>
                 </div>
               </form>
-     </div>
-  </div>
-</div>
-
-<footer class="container-fluid">
-           <p align="center"> Copyright &copy; 2018 &mdash; UPOU-CMSC-207-Team-Alpha </p>
-</footer>
-       
+            </div>
+          </div>
+          <div class="footer">
+            Copyright &copy; 2018 &mdash; UPOU-CMSC-207-Team-Alpha 
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </body>
 </html>
