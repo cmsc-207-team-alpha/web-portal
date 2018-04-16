2ced90899cf0c5bb399a433b916aefd503f1034e
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-16 22:42:49 +0800

    Update dashboard.php

--- app/dashboard.php
+++ app/dashboard.php
@@ -1,14 +1,3 @@
-/* <?php
-session_start();
-
-      if(!isset($_SESSION['admin']))
-       {
-           header("Location:login.php");  
-       }else{
-	      header("Location:fare/index.php");
-      }
-?> */
-
 <!DOCTYPE html>
 <html lang="en">
 	<head>
