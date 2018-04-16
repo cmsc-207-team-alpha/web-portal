3878d9f6c3592c07cd4d734b4889bacf2c514186
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-17 00:26:45 +0800

    Update map.php

--- app/map.php
+++ app/map.php
@@ -1,6 +1,10 @@
 <?php
-session_start();
+if (!isset($_SESSION["admin_id"]) || !isset($_SESSION["admin_name"]))
+{
+   header("location: logout.php");
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
