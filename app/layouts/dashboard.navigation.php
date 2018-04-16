cf7624d6d6b04222b7d2b9d0cea48c8a1d7019d9
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-17 00:59:53 +0800

    Update dashboard.navigation.php

--- app/layouts/dashboard.navigation.php
+++ app/layouts/dashboard.navigation.php
@@ -24,6 +24,9 @@
           <li class="nav-item">
             <a class="nav-link" href="report.php">Reports</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="register.php">Add Administrator</a>
+          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION["admin_name"]; ?></a>
             <div class="dropdown-menu dropdown-menu-right pull-right p-0" aria-labelledby="dropdown04">
