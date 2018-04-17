75f2c21e973e982798cbe25d3629031ce8e2ed6c
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-18 00:36:45 +0800

    Update gridtest.php

--- app/gridtest.php
+++ app/gridtest.php
@@ -15,7 +15,7 @@
 
 	        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
 						<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2">
-							<h1 class="h2"><span class="fa fa-fw fa-users"></span> my humps</h1>
+							<h1 class="h2"><span class="fa fa-fw fa-users"></span> sleepy</h1>
 								<div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
 
 								</div>
@@ -26,15 +26,15 @@
 							<table class="table table-striped table-sm" id="drivers_table">
 								<thead>
 									<tr>
-										<th>#</th>
-										<th>Driver</th>
-										<th>Email</th>
-										<th>Mobile</th>
-										<th>Verified</th>
-										<th>Blocked</th>
-										<th>Active</th>
-										<th>Rating</th>
-										<th>Actions</th>
+										<th>field_1</th>
+										<th>field_2</th>
+										<th>field_3</th>
+										<th>field_4</th>
+										<th>field_5</th>
+										<th>field_6</th>
+										<th>field_7</th>
+										<th>field_8</th>
+										<th>field_9</th>
 									</tr>
 								</thead>
 								<tbody id="drivers_tbl">
@@ -73,24 +73,6 @@ function load_drivers() {
 				'<td>'+verified+'</td>'+
 				'<td>'+blocked+'</td>'+
 				'<td>'+stat+'</td>'+
-				'<td>3.5</td>'+
-				'<td>'+
-					'<button class="btn btn-sm btn-default" onclick="get_driver('+data.id+')" title="View Record" data-toggle="tooltip">'+
-						'<span class="fa fa-eye"></span> '+
-					'</button>'+
-					'<button class="btn btn-sm btn-default" onclick="edit_driver('+data.id+')"  title="Update Record" data-toggle="tooltip">'+
-						'<span class="fa fa-edit"></span>'+
-					'</button>'+
-					'<button class="btn btn-sm btn-default" onclick="update_stats('+data.id+')" title="Update Status" data-toggle="tooltip">'+
-						'<span class="fa fa-user"></span> '+
-					'</button>'+
-					'<button class="btn btn-sm btn-default" onclick="add_document('+data.id+')" title="Add Document" data-toggle="tooltip">'+
-						'<span class="fa fa-upload"></span>'+
-					'</button>'+
-					'<button class="btn btn-sm btn-default" onclick="delete_driver('+data.id+')" title="Delete Record" data-toggle="tooltip">'+
-						'<span class="fa fa-trash"></span>'+
-					'</button>'+
-				'</td>'+
 			'</tr>');
     		
 		});
