033442234a78c89fee9ec3188cb055c8d735932b
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-17 16:11:59 +0800

    Update gridtest.php

--- app/gridtest.php
+++ app/gridtest.php
@@ -14,11 +14,11 @@
 		<link rel="stylesheet" href="../app/assets/stylesheets/datatables.min.css">
 	</head>
 	<body class="login-page">
-		<?php include_once("layouts/dashboard.navigation.php") ?>
+
 		<script src="../app/assets/js/datatables.min.js"></script>
 	    <div class="container-fluid">
 	      <div class="row">
-	      	<?php include_once("layouts/dashboard.sidebar.php") ?>
+
 	        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
 						<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2">
 							<h1 class="h2"><span class="fa fa-fw fa-chart-line"></span> Reports</h1>
@@ -31,12 +31,12 @@
 							<table class="table table-striped table-sm" id="payment_table">
 								<thead>
 									<tr>
-										<th>ID</th>
-										<th>Trip ID</th>
-										<th>Date</th>
-										<th>Mode</th>
-										<th>Amount</th>
-										<th>Actions</th>
+										<th>field_1</th>
+										<th>field_2</th>
+										<th>field_3</th>
+										<th>field_4</th>
+										<th>field_5</th>
+										<th>field_6</th>
 									</tr>
 								</thead>
 								<tbody id="payment_tbl">
