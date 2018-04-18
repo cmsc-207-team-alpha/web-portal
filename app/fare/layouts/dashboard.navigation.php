1e51403fc0e00c2888687681583c88824f021c95
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-18 18:34:59 +0800

    added administrator link

--- app/fare/layouts/dashboard.navigation.php
+++ app/fare/layouts/dashboard.navigation.php
@@ -32,7 +32,7 @@ if (!isset($_SESSION["admin_id"]) || !isset($_SESSION["admin_name"]))
             <a class="nav-link" href="/app/report.php">Reports</a>
           </li>
            <li class="nav-item">
-            <a class="nav-link" href="Administrators.php">Administrators</a>
+            <a class="nav-link" href="/app/Administrators.php">Administrators</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION["admin_name"]; ?></a>
