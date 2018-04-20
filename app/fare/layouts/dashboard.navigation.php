f0fe6a31b8f8322a61deb4e1e2b6125a567114aa
etpimentel <37646409+etpimentel@users.noreply.github.com>
2018-04-20 19:52:08 +0800

    Update dashboard.navigation.php

--- app/fare/layouts/dashboard.navigation.php
+++ app/fare/layouts/dashboard.navigation.php
@@ -40,8 +40,6 @@ if (!isset($_SESSION["admin_id"]) || !isset($_SESSION["admin_name"]))
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION["admin_name"]; ?></a>
             <div class="dropdown-menu dropdown-menu-right pull-right p-0" aria-labelledby="dropdown04">
-              <a class="dropdown-item" href="#">Account Settings</a>
-              <a class="dropdown-item" href="#">Help</a>
               <a class="dropdown-item" href="../logout.php">Sign Out</a>
             </div>
           </li>
