e6ab7002ff4a0de59fe90b84d4dbfde7d988fc63
amperrj <37677259+amperrj@users.noreply.github.com>
2018-05-07 08:09:13 +0800

    Update login.php

--- app/login.php
+++ app/login.php
@@ -13,25 +13,29 @@ session_start();
 </head>
 <body class="login-page">
   <section class="h-100">
-    <div class="container h-100">
+   <div class="container h-100">
       <div class="row  h-100">
           <div class="brand-lg">
             <a href="index.php">
               <img src="assets/images/logoteam-alpha.png">
-            </a>
+            <a href="index.php">
           </div>
+          <div class="card-wrapper">
+            <div class="brand">
+              <a href="index.php">
+                <img src="assets/images/logoteam-alpha.png">
+              </a>
+            </div>
           <div class="card fat">
-            <div class="card-body">
+              <div class="card-body">
               <h4 class="card-title">Administrator Login</h4>
-			  
-			  	<div class="form-group col-md-8">
-					<div id="result">			
-					</div>
-				</div>
+        
+              <div class="form-group col-md-8">
+                  <div id="result">
+                  </div>
+              </div>
               
                 <div class="form-group">
-			<p></p>
-			<p></p>
                   <label for="email">Email Address</label>
                   <div class="input-group mb-2 mr-sm-2">
                   <div class="input-group-prepend">
@@ -54,35 +58,35 @@ session_start();
                   </div>
                 </div>
 
-<?php
-    if(isset($_COOKIE["login_hold"])){
-    echo 'Youre not allowed to login for 30 minutes<br/>';
-}
-else {    
-    if(isset($_POST["submit"])){    
-        if(!empty($_POST['email']) && !empty($_POST['password'])) {  
-            $email=$_POST['email'];  
-            $password=$_POST['password'];
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {  
-                    $_SESSION['sess_user']=$row['email'];
-                    echo 'You are currently logged in';
+                <?php
+                    if(isset($_COOKIE["login_hold"])){
+                    echo 'Youre not allowed to login for 30 minutes<br/>';
                 }
-            }else {  
-                echo "Invalid email address or password!<br/>";  
-                $_SESSION['login_attempts'] = $_SESSION['login_attempts'] + 1;
-                echo 3 -  $_SESSION['login_attempts'] . ' attempt/s remaining.';
-                if($_SESSION['login_attempts'] >= 3){
-                    setcookie("login_hold", "hold", time() + (1800));
-                    $_SESSION['login_attempts'] = 0; 
+                else {    
+                    if(isset($_POST["submit"])){    
+                        if(!empty($_POST['email']) && !empty($_POST['password'])) {  
+                            $email=$_POST['email'];  
+                            $password=$_POST['password'];
+                            if ($result->num_rows > 0) {
+                                while($row = $result->fetch_assoc()) {  
+                                    $_SESSION['sess_user']=$row['email'];
+                                    echo 'You are currently logged in';
+                                }
+                            }else {  
+                                echo "Invalid email address or password!<br/>";  
+                                $_SESSION['login_attempts'] = $_SESSION['login_attempts'] + 1;
+                                echo 3 -  $_SESSION['login_attempts'] . ' attempt/s remaining.';
+                                if($_SESSION['login_attempts'] >= 3){
+                                    setcookie("login_hold", "hold", time() + (1800));
+                                    $_SESSION['login_attempts'] = 0; 
+                                }
+                            }  
+                        } else {  
+                            echo "All fields are required!";  
+                        }  
+                    }
                 }
-            }  
-        } else {  
-            echo "All fields are required!";  
-        }  
-    }
-}
-?>
+                ?>
 
                 <div class="form-group no-margin">
                   <button type="submit" class="btn btn-success btn-block" onclick="login()">
@@ -92,8 +96,7 @@ else {
             </div>
           </div>
           <div class="footer">
-            Copyright &copy; 2018 &mdash; UPOU-CMSC-207-Team-Alpha
- 
+            Copyright &copy; 2018 &mdash; UPOU-CMSC-207-Team-Alpha 
           </div>
         </div>
       </div>
