df9f296f456dfd91675d5199047ab1c75822c58a
turko09 <37707972+turko09@users.noreply.github.com>
2018-05-09 12:12:02 +0800

    Create activationlink.php

---
+++ app/activationlink.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include_once("layouts/header.php") ?>
+<script src="assets/js/common.js"></script>
+<script src="assets/js/login.js"></script>
+  <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+<script src="assets/lib/jquery/js/jquery.min.js"></script>
+</head>
+<body class="login-page">
+  <section class="h-100">
+   <div class="container h-100">
+      <div class="row  h-100">
+          <div class="brand-lg">
+            <a href="index.php">
+              <img src="assets/images/logoteam-alpha.png">
+            <a href="index.php">
+          </div>
+          <div class="card-wrapper">
+            <div class="brand">
+              <a href="index.php">
+                <img src="assets/images/logoteam-alpha.png">
+              </a>
+            </div>
+          
+</body>
+</html>
